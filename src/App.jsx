f2fc080208f132a6c11f73915fe7b6d6861024fb
Vương Hồng Lĩnh <linhhandsom@gmail.com>
2023-11-06 00:54:50 +0700

    fix: link error

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           <Route path="/smart-printing-service/login" element={<LoginPage/>} />
           <Route path='/smart-printing-service/*' element={<NotFound/>}></Route>
           <Route path="/smart-printing-service" element={<Root/>}>
-            <Route path="/" element={<HomePage/>} />
+            <Route path="/smart-printing-service" element={<HomePage/>} />
             <Route path="/smart-printing-service/LichSu" element={<HistoryPage/>} />
             <Route path="/smart-printing-service/ThanhToan" element={<PaymentPage/>} />
             <Route path="/smart-printing-service/PhanHoi" element={<FeedbackPage/>} />
