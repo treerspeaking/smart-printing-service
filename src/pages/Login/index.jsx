21a69e264cfa43e08fea259bb4ebffc4b2b815f3
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-11-12 19:14:17 +0700

    refactor: separete the page content and provider

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,27 +1,14 @@
-import { Box, ImageList, ImageListItem, Stack, Typography } from '@mui/material';
 import React from 'react';
-import { LoginForm } from './LoginForm';
-import { Image } from '@mui/icons-material';
-import { Container } from '@mui/material';
-const logo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/HCMUT_official_logo.png/800px-HCMUT_official_logo.png'
+import { LoginPageContent } from './LoginPageContent';
+
+import { AuthProvider } from "../../contexts/AuthContext";
+
 export default function LoginPage() {
   return (
-    <Box sx={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <Container sx={{ width:"100%"}} >
-        <Stack direction={'row'} sx={{justifyContent:'center', display:'flex'}}spacing={10}>
-          <Stack direction={'column'} spacing={5} flexGrow={2} justifyContent="center">
-            <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px' }}>
-              <img
-                src={logo}
-                style={{ maxWidth: '400px', maxHeight: '300px' }}
-              ></img>
-            </Container>
-            <Typography variant='h3' textAlign="center" width={500} sx={{ color: '#4B95DA' }}>SMART STUDENT <br /> PRINTING SERVICE</Typography>
-          </Stack>  
-          <LoginForm/>
-        </Stack>
-      </Container>
-    </Box>
+
+    <AuthProvider>
+      <LoginPageContent />
+    </AuthProvider>
   );
 }
 
