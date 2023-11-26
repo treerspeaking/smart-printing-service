308116849e5989163d113de99f32a00014616e96
Khoa LTN <vietnamkhoale@gmail.com>
2023-11-26 10:15:46 +0700

    docs: add diagrams in docs/

---
+++ docs/diagrams/README.md
@@ -0,0 +1,5 @@
+Only people who have source.txt in each folder is able to fetch the files.
+
+To fetch the files, create a file drawio.txt in this directory, and paste path_to_drawio_app into this file.
+
+Remember to `cd docs` before executing update.sh.
---
+++ docs/diagrams/update.sh
@@ -0,0 +1,42 @@
+drawio=$(cat "$PWD/drawio.txt")
+
+function fetch_and_compile() {
+    curl -L "https://drive.google.com/uc?export=download&id=${1}" -o "${1}.drawio"
+
+    # Count how many pages based on <diagram element
+    count=$(grep -o "<diagram" "${1}.drawio" | wc -l)
+
+    # Process and get the list of diagram's namesa
+    strings=$(cat ${1}.drawio | grep -o 'name="[^"]*"' | awk -F'"' '{print $2}')
+    names=($strings)
+
+    # Export each page as an PNG
+    # Page index is zero based
+    for ((i = 0; i <= $count - 1; i++)); do
+        eval "$drawio --export --crop --transparent --page-index $i --output "${names[i]}.pdf" --format "pdf" "${1}.drawio""
+    done
+}
+
+function source_from() {
+    IFS=$'\n' read -d '' -r -a download_srcs <${1}
+
+    for src in ${download_srcs[@]}; do
+        fetch_and_compile ${src}
+    done
+}
+
+# source.txt is only available on
+
+cd task1
+source_from "source.txt"
+cd ../
+
+cd task2
+source_from "source.txt"
+cd ../
+
+cd task3
+source_from "source.txt"
+cd ../
+
+find . -type f -iname "*.drawio" -delete
