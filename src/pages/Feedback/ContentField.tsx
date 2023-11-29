569f51f8045b1ee758cff0b1127d6664b6a27546
Quessalini03 <vuchauduyquang@gmail.com>
2023-11-29 10:11:33 +0700

    visual: font weights in feedback

--- src/pages/Feedback/ContentField.tsx
+++ src/pages/Feedback/ContentField.tsx
@@ -6,12 +6,12 @@ export const ContentField = () => {
     return (
         <Box sx ={{backgroundColor: '#f8f8f8', padding: '16px', height: '475px', position:'bottom', borderRadius:'25px'}} boxShadow={5}>
             <Stack direction='column' spacing='20px'>
-                <Typography variant='h5' textAlign="left" width={500} sx={{ color: '#000000' }}> Phản hồi của bạn</Typography>
+                <Typography variant='h4' textAlign="left" fontWeight={700} width={500} sx={{ color: '#000000' }}> Phản hồi của bạn</Typography>
 
-                <Typography variant='h6' textAlign="left" width={500} sx={{ color: '#000000' }}> Tiêu đề</Typography>
-                <TextField multiline={true} minRows='1' ></TextField>
+                <Typography variant='h6' textAlign="left" fontWeight={700} width={500} sx={{ color: '#000000' }}> Tiêu đề</Typography>
+                <TextField multiline={true} minRows='1'></TextField>
 
-                <Typography variant='h6' textAlign="left" width={500} sx={{ color: '#000000' }}> Nội dung</Typography>
+                <Typography variant='h6' textAlign="left" fontWeight={700} width={500} sx={{ color: '#000000' }}> Nội dung</Typography>
                 <TextField multiline={true} minRows='5'></TextField>
 
                 <SubmitButton />
