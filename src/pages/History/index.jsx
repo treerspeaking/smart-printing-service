ab6e044e5c6b8047c891850d42e21fcd0dd92cb6
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-12-02 16:19:56 +0700

    Update index.jsx

--- src/pages/History/index.jsx
+++ src/pages/History/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ColumnGroupingTable from '../Home/Table';
 import { Stack, Typography } from '@mui/material';
+import { PrinterProvider } from '../../contexts/PrinterContext';
 export default function HomePage() {
   return (
-      <Stack direction={'column'} spacing={'83px'}>
-        <Typography variant='h3'>LỊCH SỬ IN</Typography>
-        <ColumnGroupingTable></ColumnGroupingTable>
-      </Stack>
+      <PrinterProvider>
+        <Stack direction={'column'} spacing={'83px'}>
+          <Typography variant='h3'>LỊCH SỬ IN</Typography>
+          <ColumnGroupingTable></ColumnGroupingTable>
+        </Stack>
+      </PrinterProvider>
   );
 }
 
