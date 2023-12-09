7665f0b19540308fac848825a6d15c676e7b5b54
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-12-09 22:00:04 +0700

    fix: printing request link

--- src/pages/Home/PrintRequest.tsx
+++ src/pages/Home/PrintRequest.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Button } from '@mui/material'
 import {Link} from 'react-router-dom'
+
+import { ROUTES } from '../../contexts/RouteContext'
+
 export const PrintRequest = () => {
   return (
     
-    <Link to={'/smart-printing-service/PrintRequest'}>
+    <Link to={ROUTES.PRINT_REQUEST}>
       <Button variant='contained' sx={{width:161, height:57, borderRadius:100}} >Đặt in mới</Button>
     </Link>
   )
